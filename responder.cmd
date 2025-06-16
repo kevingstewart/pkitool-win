e6770fb1595b40fc989cabb14a22d46f6ff36907
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-06-16 13:51:30 -0400

    Update responder.cmd

--- responder.cmd
+++ responder.cmd
@@ -1,5 +1,5 @@
 rem ===================================================
-rem Author: k.stewart@f5.com
+rem Author: kevin-at-f5-dot-com
 rem ===================================================
 
 openssl ocsp -index PROCESS\subca1.f5demolab.com\index.txt -port 8888 -CA CA\subca1.f5demolab.com.cer -rsigner SERVERCERTS\subca1.f5demolab.com\ocsp.f5demolab.com.crt -rkey SERVERCERTS\subca1.f5demolab.com\ocsp.f5demolab.com.key -text -out responder.log.txt
